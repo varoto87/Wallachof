a164d4a675839f2f5b8bb31058e24bf1269f8561
David <david@mail.com>
2019-05-20 16:35:18 +0200

    Initial Commit

---
+++ Wallachof/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wallachof
+//
+//  Created by Dev2 on 20/05/2019.
+//  Copyright © 2019 Dev2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
