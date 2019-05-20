d3600f3a73815894af51cf6aecac878da9bff70e
David <david@mail.com>
2019-05-20 20:04:58 +0200

    prueba de creación de un objeto de coreada

--- Wallachof/ViewController.swift
+++ Wallachof/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        UserDefaults.standard.set("Hola", forKey: "Mundo")
     }
 
 
+    @IBAction func btnTestPressed(_ sender: Any) {
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let persistentContainer = appDelegate.persistentContainer
+        let context = persistentContainer.viewContext
+        
+        let dinoDetector = Product(context: context)
+        dinoDetector.name = "Dino Detector Ultimate 🦖"
+        dinoDetector.desc = "Detector de dinosaurios de última generación"
+        dinoDetector.price = 2000.0
+        
+        appDelegate.saveContext()
+        
+    }
 }
 
